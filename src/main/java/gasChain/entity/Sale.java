b2c07b10046a63e82066de88d9c267c57eeaaef4
Dallas Evers <Dallas.Evers@gmail.com>
2019-10-29 23:39:34 -0500

    Sale changes (wip)

--- src/main/java/gasChain/entity/Sale.java
+++ src/main/java/gasChain/entity/Sale.java
@@ -1,57 +1,38 @@
 package gasChain.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.sql.Date;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
 @Entity
 public class Sale {
-	
-	@Id
+
+    @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
-	
-	private String type;
-//	private String typeClass ??? - Group items into classes eg Food, drinks, etc
-	private float price;
-	private Date sellDate;
+
+    @NotNull
+    private float price;
+
+    private Date sellDate;
 
     @ManyToOne
-    @JoinColumn(name = "sellLocation_id")
+    @NotNull
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @NotNull
+    @JoinColumn(name = "gas_station_id")
     private GasStation sellLocation;
-	
-    // Constructors
-    protected Sale() {}
-    public Sale(String type, float price, Date sellDate)
-    {
-    	this.type = type; this.price = price; this.sellDate = sellDate;
-    }
-    public Sale(String type, float price, Date sellDate, GasStation sellLocation)
-    {
-    	this(type, price, sellDate); this.sellLocation = sellLocation;
-    }
-    public Sale(Sale sale)
-    {
-    	this(sale.getType(), sale.getPrice(), sale.getSellDate()); 
-    }
-    public Sale(Sale sale, GasStation sellLocation)
-    {
-    	this(sale); this.sellLocation = sellLocation;
-    }
-    public Sale(Sale sale, Date sellDate, GasStation sellLocation)
-    {
-    	this(sale); this.sellDate = sellDate; this.sellLocation = sellLocation;
-    }
-    
-    public String getType() { return type; }
-    public float getPrice() { return price; }
-    public Date getSellDate() { return sellDate; }
-    public GasStation getSellLocation() { return sellLocation; }
-    public Sale addSellLocation(GasStation sellLocation) { this.sellLocation = sellLocation; sellLocation.addSale(this); return this; }
-    
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/gasChain/entity/WarehouseInventory.java
+++ src/main/java/gasChain/entity/WarehouseInventory.java
@@ -30,6 +30,7 @@ public class WarehouseInventory  implements Serializable {
         this.price = price;
         this.quantity = quantity;
     }
+
     public Item getItem() {
         return item;
     }
